667126944ebdd5c22266e2318cff8f28bf7451cd
n-dohrmann <noah@dohrmann.org>
2026-06-21 13:59:13 -0400

    add u32_t to local include
    Signed-off-by: n-dohrmann <noah@dohrmann.org>

--- components/local_include.h
+++ components/local_include.h
@@ -2,5 +2,10 @@
 // misc defines that were not pre-included anywhere else
 
 #ifndef CONFIG_FREERTOS_HZ
-#define CONFIG_FREERTOS_HZ 1000 // 1000 is the default...
+#define CONFIG_FREERTOS_HZ 1000
 #endif // CONFIG_FREERTOS_HZ
+
+#ifndef _U32_T_IMPL
+#define _U32_T_IMPL
+typedef unsigned int u32_t;
+#endif // _U32_T_IMPL
