8bd6e69905e31cd10b8f93b41048be1bdf0072f9
n-dohrmann <noah@dohrmann.org>
2026-06-21 19:03:19 -0400

    fix: pin definitions
    Signed-off-by: n-dohrmann <noah@dohrmann.org>

--- components/local_dev/include/esp01s_pins.h
+++ components/local_dev/include/esp01s_pins.h
@@ -4,25 +4,17 @@
 
 
 // numeric definitions
-#define ESP01S_GND_NUM 1
+#define ESP01S_GPIO0_NUM 0
+#define ESP01S_TX_NUM 1
 #define ESP01S_GPIO2_NUM 2
-#define ESP01S_GPIO0_NUM 3
-#define ESP01S_RX_NUM 4
-#define ESP01S_VCC_NUM 5
-#define ESP01S_RST_NUM 6
-#define ESP01S_EN_NUM 7 // also called CH_PD
-#define ESP01S_TX_NUM 8
+#define ESP01S_RX_NUM 3
 
 // bitmask definitions
 #ifndef BIT
 #define BIT(x) (1 << (x))
 #endif
 
-#define ESP01S_GND_BIT BIT(1)
-#define ESP01S_GPIO2_BIT BIT(2)
-#define ESP01S_GPIO0_BIT BIT(3)
-#define ESP01S_RX_BIT BIT(4)
-#define ESP01S_VCC_BIT BIT(5)
-#define ESP01S_RST_BIT BIT(6)
-#define ESP01S_EN_BIT BIT(7)
-#define ESP01S_TX_BIT BIT(8)
+#define ESP01S_GPIO0_BIT BIT(ESP01S_GPIO0_NUM)
+#define ESP01S_TX_BIT BIT(ESP01S_TX_NUM)
+#define ESP01S_GPIO2_BIT BIT(ESP01S_GPIO2_NUM)
+#define ESP01S_RX_BIT BIT(ESP01S_RX_NUM)
